19235c049b268b3fd38e7ebbed4087c76ddc1b92
Antony Nascimento <Oh20tony@outlook.com>
2024-09-23 22:22:12 -0300

    ok

--- DB_Consumer/obj/Debug/net8.0/DB_Consumer.AssemblyInfo.cs
+++ DB_Consumer/obj/Debug/net8.0/DB_Consumer.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DB_Consumer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+07b4bad162d3a714071e22c360453231a75d80b8")]
 [assembly: System.Reflection.AssemblyProductAttribute("DB_Consumer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DB_Consumer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
